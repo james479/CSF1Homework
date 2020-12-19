085ab96418fa572c05061a32aef074a644a1805c
James Glover <james.e.glover1@gmail.com>
2020-12-18 21:31:59 -0600

    finished FruitLoop1.cs

---
+++ CSF1Homework/FruitLoop1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop1
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = { "Apple", "Grape", "Orange", "Pear", "Grapefruit", "Avocado"  };
+
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
