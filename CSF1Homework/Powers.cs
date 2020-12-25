efe7e85fd2dfacaa1969f9e496d3308b1f777779
James Glover <james.e.glover1@gmail.com>
2020-12-24 20:43:54 -0600

    finished Power.cs

---
+++ CSF1Homework/Powers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Powers
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                long power = Convert.ToInt64(Math.Pow(i, 2));
+                //displaying result
+                Console.WriteLine($"2 to the {i} power is {power}.");
+                
+
+            }
+        }
+    }
+}
